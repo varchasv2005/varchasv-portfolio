553d268e4113c258fd267033573bb311407b9d13
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2025-08-21 23:58:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/varchasv-portfolio/',   // 👈 add this
+})
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
